747008ebf2be976d66ea0fee02c75056ccce9797
Silio Freitas <siliosffreitas@gmail.com>
2024-10-10 13:33:49 -0300

    add tests for AddAccount

---
+++ test/features/signin/domain/usecases/add_account_test.dart
@@ -0,0 +1,43 @@
+import 'package:tasks_app/features/auth/domain/entities/index.dart';
+import 'package:tasks_app/features/auth/domain/repositories/index.dart';
+import 'package:tasks_app/features/auth/domain/usecases/authentication.dart';
+import 'package:tasks_app/features/signin/domain/repositories/add_account_repository.dart';
+import 'package:tasks_app/features/signin/domain/usecases/add_account.dart';
+import 'package:test/test.dart';
+
+import 'package:mocktail/mocktail.dart';
+import 'package:faker/faker.dart';
+import 'package:dartz/dartz.dart';
+
+class MockAddAccountRepository extends Mock implements AddAccountRepository {}
+
+void main() {
+  late AddAccount usecase;
+  late AddAccountRepository repository;
+
+  late AccountEntity tAccount;
+  late String tUsername;
+  late String tPassword;
+
+  setUp(() {
+    repository = MockAddAccountRepository();
+    usecase = AddAccountImp(repository: repository);
+    tAccount = AccountEntity(accessToken: faker.guid.guid());
+    tUsername = faker.internet.email();
+    tPassword = faker.internet.password();
+  });
+
+  test('Should get account from the repository', () async {
+    when(() => repository.signin(any(), any()))
+        .thenAnswer((_) async => Right(tAccount));
+
+    final result = await usecase(AddAccountParams(
+      username: tUsername,
+      password: tPassword,
+    ));
+
+    expect(result, Right(tAccount));
+    verify(() => repository.signin(tUsername, tPassword)).called(1);
+    verifyNoMoreInteractions(repository);
+  });
+}

53649c2be493813df48f6b855848d185a7b106fd
Silio Freitas <siliosffreitas@gmail.com>
2024-10-10 02:48:06 -0300

    create implementation to usecase

--- lib/features/home/domain/usecases/logout.dart
+++ lib/features/home/domain/usecases/logout.dart
@@ -1,3 +1,17 @@
+import 'package:tasks_app/core/error/failures.dart';
+
+import 'package:dartz/dartz.dart';
+
 import '../../../../core/usecases/usecase.dart';
+import '../repositories/logout_repository.dart';
 
 abstract class Logout implements UseCase<void, NoParams> {}
+
+class LogoutImp implements Logout {
+  final LogoutRepository repository;
+
+  LogoutImp({required this.repository});
+
+  @override
+  Future<Either<Failure, void>> call(NoParams params) => repository.logout();
+}
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -8,6 +8,7 @@ import 'features/auth/data/repositories/authentication_repository_impl.dart';
 import 'features/auth/domain/repositories/authentication_repository.dart';
 import 'features/auth/domain/usecases/authentication.dart';
 import 'features/auth/presentation/presenters/mobx_login_presenter.dart';
+import 'features/home/domain/repositories/logout_repository.dart';
 import 'features/home/domain/usecases/logout.dart';
 import 'features/home/presentation/presenters/mobx_home_presenter.dart';
 import 'features/signin/data/datasources/add_account_remote_datasource.dart';
@@ -43,6 +44,8 @@ class AppModule extends Module {
             repository: Modular.get<AuthenticationRepository>())),
         Bind<AddAccount>((_) =>
             AddAccountImp(repository: Modular.get<AddAccountRepository>())),
+        Bind<Logout>(
+            (_) => LogoutImp(repository: Modular.get<LogoutRepository>())),
 
         // repositories
         Bind<CheckHasLoggedUserRepository>((_) =>

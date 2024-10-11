9b135f7caad878fdf2277529c240d1c1bbb5e2e7
Silio Freitas <siliosffreitas@gmail.com>
2024-10-11 13:40:58 -0300

    add tests for CheckHasLoggedUserRemoteDataSource

---
+++ test/features/splash/data/datasources/check_has_logged_user_remote_datasource_test.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tasks_app/features/auth/data/models/account_model.dart';
+import 'package:tasks_app/features/splash/data/datasources/check_has_logged_user_remote_datasource.dart';
+import 'package:test/test.dart';
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockUser extends Mock implements User {
+  @override
+  String? get email => 'siliosffreitas@gmail.com';
+}
+
+void main() {
+  late CheckHasLoggedUserRemoteDataSourceFirebase sut;
+  late FirebaseAuth firebase;
+  late MockUser tUser;
+
+  setUp(() {
+    firebase = MockFirebaseAuth();
+    sut = CheckHasLoggedUserRemoteDataSourceFirebase(firebase);
+    tUser = MockUser();
+  });
+
+  test('Should call firebase currentuser ', () async {
+    await sut.check();
+    verify(() => firebase.currentUser).called(1);
+  });
+
+  test('Should return null if firebase currentuser is null', () async {
+    when(() => firebase.currentUser).thenReturn(null);
+    final result = await sut.check();
+    expect(result, isNull);
+  });
+
+  test('Should return account if firebase currentuser is not null', () async {
+    when(() => firebase.currentUser).thenReturn(tUser);
+    final result = await sut.check();
+    expect(result, const AccountModel(accessToken: 'siliosffreitas@gmail.com'));
+  });
+}

faace3c7cb052d41ef0ad2c7498d4933c95016e2
Silio Freitas <siliosffreitas@gmail.com>
2024-11-21 10:16:33 -0300

    some tests

--- integration_test/login_feature_test.dart
+++ integration_test/login_feature_test.dart
@@ -12,10 +12,49 @@ void main() {
     await tester.pumpAndSettle();
 
     await tester.tap(find.byType(TextButton));
-    await Future.delayed(const Duration(seconds: 1));
 
     await tester.pumpAndSettle();
 
     expect(find.text('Crie sua conta fácil'), findsOneWidget);
+  }));
+
+  testWidgets('Should login with success', ((WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.bySemanticsLabel('E-mail'), 'siliosffreitas@gmail.com');
+    await tester.enterText(find.bySemanticsLabel('Senha'), 'Silio123\$');
+
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Tarefas'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.exit_to_app));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('Sim'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Bem-vindo ao Lista de tarefas by Silio'), findsOneWidget);
+  }));
+
+  testWidgets('Should login with fails', ((WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.bySemanticsLabel('E-mail'), 'invalid@email.com');
+    await tester.enterText(find.bySemanticsLabel('Senha'), 'invalid_pass');
+
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Tarefas'), findsNothing);
+    expect(find.text('Atenção'), findsOneWidget);
   }));
 }
